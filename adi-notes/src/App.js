5d50d35ad9d94a755dcc2bbf937b164f2dfd67c1
Adrian Adona <95672279+AAdona@users.noreply.github.com>
2022-02-24 20:56:27 +0800

    firebase

--- adi-notes/src/App.js
+++ adi-notes/src/App.js
@@ -1,6 +1,29 @@
+import * as React from 'react';
 import './App.css';
 
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp = {
+  apiKey: "AIzaSyAB4PpGX2y9eOpD0Z-UGfXDpJ4ZCEN9Uuw",
+  authDomain: "adi-notes-3d984.firebaseapp.com",
+  projectId: "adi-notes-3d984",
+  storageBucket: "adi-notes-3d984.appspot.com",
+  messagingSenderId: "1049074837941",
+  appId: "1:1049074837941:web:2369ec17c3ea9065af51fc",
+  measurementId: "G-J4RTEWK9SH"
+};
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 function App() {
+  const [user] = useAuthState(auth);
   return (
    "Hello"
   );
